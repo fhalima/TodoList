485822980bd50ae978a6cfd48a32a08a7b865503
halima <ghazaloran2@gmail.com>
2021-10-03 21:46:20 +0200

    BackEnd Server Express

---
+++ client/src/App.js
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import "./App.css";
+
+function App() {
+  const [titre, setTitre] = useState("");
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/get").then((response) => {
+      setTodoList(response.data);
+    });
+  }, []);
+
+  const submitTodo = (e) => {
+    e.preventDefault();
+    const colorRandom = "#" + ((Math.random() * 0xeeeeee) << 0).toString(16);
+    Axios.post("http://localhost:3001/api/insert", {
+      titre: titre,
+      todo: todo,
+    }).then(() =>
+      setTodoList([
+        ...todoList,
+        {
+          titre: titre,
+          todo: todo,
+          color: colorRandom,
+        },
+      ])
+    );
+    console.log(todoList);
+  };
+
+  const deleteTodo = (titre) => {
+    Axios.delete(`http://localhost:3001/api/delete/${titre}`).then(
+      deleteElm(titre)
+    );
+  };
+  const deleteElm = (titre) => {
+    setTodoList(todoList.filter((todoelm) => todoelm.titre !== titre));
+  };
+  return (
+    <div className="App container">
+      <h1> Todos List </h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="titre"> Titre </label>
+          <input
+            type="text"
+            className="form-control"
+            id="titre"
+            name="titre"
+            onChange={(e) => {
+              setTitre(e.target.value);
+            }}
+          />
+          <label htmlFor="todo"> Todo </label>
+          <input
+            type="text"
+            className="form-control"
+            id="todo"
+            name="todo"
+            onChange={(e) => {
+              setTodo(e.target.value);
+            }}
+          />
+          <button className="btn btn-primary" onClick={submitTodo}>
+            Ajouter
+          </button>
+        </div>
+      </form>
+      {todoList.map((val) => {
+        return (
+          <div
+            className="section m-2 "
+            style={{
+              background: val.color,
+            }}
+          >
+            <div className="row mx-auto" key={val.titre}>
+              <div className="col mx-auto df">
+                <p>
+                  <span>
+                    <strong>
+                      {val.titre} {": "}
+                    </strong>
+                  </span>
+                  {val.todo}
+                </p>
+              </div>
+              <div
+                type="button"
+                className=" text-center btn btn-danger bt col-1"
+                onClick={() => {
+                  deleteTodo(val.titre);
+                }}
+              >
+                <i
+                  className="mx-auto  fa fa-window-close"
+                  aria-hidden="true"
+                ></i>
+              </div>
+            </div>
+            {/* <input type="text" />
+            <button> Update </button> */}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+let jsonData = require("./data.json");
+
+const app = express();
+let datajson;
+
+const getDataJson = () => {
+    fs.readFile("./data.json", (err, data) => {
+        if (err) {
+            throw err;
+            datajson = [""];
+        } else datajson = JSON.parse(data);
+    });
+};
+const setDataJson = () => {
+    let jd = JSON.stringify(datajson);
+
+    fs.writeFile("data.json", jd, () => {});
+};
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/api/get", (req, response) => {
+    getDataJson();
+    response.send(datajson);
+});
+
+app.delete("/api/delete/:titre", (req, res) => {
+    let key = req.params.titre;
+
+    for (var i = datajson.length - 1; i >= 0; i--) {
+        if (datajson[i].titre === key) {
+            datajson.splice(i, 1);
+        }
+    }
+    setDataJson();
+});
+app.post("/api/insert", (req, res) => {
+    const titren = req.body.titre;
+    const todon = req.body.todo;
+    const color = req.body.color;
+    let objTodo = {
+        id: Math.floor((1 + Math.random()) * 0x10000),
+        titre: titren,
+        todo: todon,
+        color: color,
+    };
+    datajson.push(objTodo);
+    setDataJson();
+});
+
+app.listen(3001, () => {
+    console.log("écoute sur le port 3001");
+});
